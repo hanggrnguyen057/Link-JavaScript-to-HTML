3256dca78b58b0226754cb65d89059146d579790
Thi Thu Hang Nguyen <hangnguyenmbi@gmail.com>
2022-09-07 10:53:07 +1000

    Second commit

--- JavatoHTML.js
+++ JavatoHTML.js
@@ -1,12 +1,6 @@
 function init() {
-    var costPerHour=prompt("Enter your name: " );
-    var chargePerHour=parseFloat(costPerHour);
-    var hours= prompt("Enter number of hours on the job: ");
-    var numHours=Number(hours);
-    var totalCost= calcServiceCost(chargePerHour, numHours);
-    document.writeln("Charge for one hour of a plumber’s visit: $"+ costPerHour+". ");
-    document.writeln("Number of hours on the job: "+ hours+" hours. ");
-    document.writeln("Total cost service: $"+totalCost);
+    var yourname=prompt("Enter your name here: ");
+    document.writeln("Hello World, my name is "+ yourname);
     }
     
     window.onload = init;
